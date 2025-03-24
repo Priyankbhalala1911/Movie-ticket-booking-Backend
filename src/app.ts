7f55f0074596163db04764ba0a23915dc6c46800
priya <priyankbhalala1911@gmail.com>
2025-03-24 17:56:11 +0530

    To change PORT number

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,13 @@ import { initialDatabase } from "./config/database";
 import UserRouter from "./routes/user";
 import cors from "cors";
 const app = express();
+import "dotenv/config";
 
 app.use(cors());
 app.use(express.json());
 app.use("/", UserRouter);
 
 initialDatabase();
-app.listen(8000, () => console.log("server is running on port 8000"));
+app.listen(process.env.PORT || 8100, () =>
+  console.log(`server is running on port ${process.env.PORT}`)
+);
