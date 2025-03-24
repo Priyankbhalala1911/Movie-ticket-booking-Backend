ba476678d27a92de1577a031814996ba991c9638
priya <priyankbhalala1911@gmail.com>
2025-03-24 13:44:01 +0530

    To create User Entity

---
+++ src/models/user.ts
@@ -0,0 +1,29 @@
+import { IsEmail, IsNotEmpty, Length, Matches } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @Column()
+  @IsNotEmpty({ message: "Name is Required" })
+  name!: string;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column()
+  @Matches(
+    /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*(),.?":{}|<>]).+$/,
+    {
+      message:
+        "Password must contain at least one uppercase letter, one lowercase letter, one numeric digit, and one special character",
+    }
+  )
+  @Length(8, 18, {
+    message: "Password must be between 8 and 18 characters long",
+  })
+  @IsNotEmpty({ message: "Password is required!" })
+  password!: string;
+}
