f5aa0d12fc955044929010649fb1a8820db22f07
priya <priyankbhalala1911@gmail.com>
2025-03-26 17:46:59 +0530

    Added logout Routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import { handleLogin, handleRegistration } from "../controllers/user";
+import {
+  handleLogin,
+  handleLogout,
+  handleRegistration,
+} from "../controllers/user";
 
 const router: Router = Router();
 
-router.post("/register", handleRegistration).post("/login", handleLogin);
+router
+  .post("/register", handleRegistration)
+  .post("/login", handleLogin)
+  .post("/logout", handleLogout);
 
 export default router;
