fee19f6d0ee45c0c59ebe50d7608a33952b45341
priya <priyankbhalala1911@gmail.com>
2025-04-01 09:33:52 +0530

    To change a value

--- src/utils/AddMovie.ts
+++ src/utils/AddMovie.ts
@@ -1,5 +1,5 @@
 import { AppSourcedata } from "../config/database";
-import { City, Movie, Screen, Theatre } from "../models/movie";
+import { City, Day, Movie, Screen, Theatre } from "../models/movie";
 import { MovieData } from "../data/MovieData";
 
 export const AddDStaticData = async () => {
@@ -12,33 +12,44 @@ export const AddDStaticData = async () => {
     movie.rating = movieData.rating;
     movie.movie_poster = movieData.image;
     movie.release_date = movieData.release_date;
+
     await AppSourcedata.getRepository(Movie).save(movie);
 
     for (const cityData of movieData.city) {
       const city = new City();
       city.name = cityData.name;
       city.movie = movie;
+
       await AppSourcedata.getRepository(City).save(city);
 
-      for (const theatreData of cityData.theatres) {
-        const theatre = new Theatre();
-        theatre.name = theatreData.name;
-        theatre.chain = theatreData.chain;
-        theatre.location = theatreData.location;
-        theatre.city = city;
-        await AppSourcedata.getRepository(Theatre).save(theatre);
+      for (const dayData of cityData.days) {
+        const day = new Day();
+        day.day = dayData.day;
+        day.city = city;
 
-        for (const screenData of theatreData.screens) {
-          const screen = new Screen();
-          screen.type = screenData.type;
-          screen.price = screenData.price;
-          screen.slots = screenData.slots;
-          screen.theatre = theatre;
-          await AppSourcedata.getRepository(Screen).save(screen);
+        await AppSourcedata.getRepository(Day).save(day);
+
+        for (const theatreData of dayData.theatres) {
+          const theatre = new Theatre();
+          theatre.name = theatreData.name;
+          theatre.chain = theatreData.chain;
+          theatre.location = theatreData.location;
+          theatre.day = day;
+
+          await AppSourcedata.getRepository(Theatre).save(theatre);
+
+          for (const screenData of theatreData.screens) {
+            const screen = new Screen();
+            screen.type = screenData.type;
+            screen.price = screenData.price;
+            screen.slots = screenData.slots;
+            screen.theatre = theatre;
+
+            await AppSourcedata.getRepository(Screen).save(screen);
+          }
         }
       }
     }
-    await AppSourcedata.getRepository(Movie).save(movie);
   }
 
   console.log("Seeding completed!");
