503f8a3de868850c37e9676328b19e7cfa8e248d
priya <priyankbhalala1911@gmail.com>
2025-04-01 09:29:39 +0530

    To Added Day entity andrelation are anthoer entity

--- src/models/movie.ts
+++ src/models/movie.ts
@@ -37,30 +37,49 @@ export class Movie {
   @CreateDateColumn({ type: "timestamp" })
   created_at!: Date;
 
-  @OneToMany(() => City, (city) => city.movie, { eager: true })
+  @OneToMany(() => City, (city) => city.movie, { eager: true, cascade: true })
   cities!: City[];
 }
 
 @Entity()
 export class City {
   @PrimaryGeneratedColumn("uuid")
-  id!: number;
+  id!: string;
 
   @Column()
   name!: string;
 
   @ManyToOne(() => Movie, (movie) => movie.cities, { onDelete: "CASCADE" })
-  @JoinColumn({ name: "movieId" })
+  @JoinColumn({ name: "movie_id" })
   movie!: Movie;
 
-  @OneToMany(() => Theatre, (theatre) => theatre.city, { eager: true })
+  @OneToMany(() => Day, (day) => day.city, { cascade: true, eager: true })
+  days!: Day[];
+}
+
+@Entity()
+export class Day {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @Column()
+  day!: number;
+
+  @ManyToOne(() => City, (city) => city.days, { onDelete: "CASCADE" })
+  @JoinColumn({ name: "city_id" })
+  city!: City;
+
+  @OneToMany(() => Theatre, (theatre) => theatre.day, {
+    cascade: true,
+    eager: true,
+  })
   theatres!: Theatre[];
 }
 
 @Entity()
 export class Theatre {
   @PrimaryGeneratedColumn("uuid")
-  id!: number;
+  id!: string;
 
   @Column()
   name!: string;
@@ -71,18 +90,21 @@ export class Theatre {
   @Column()
   location!: string;
 
-  @ManyToOne(() => City, (city) => city.theatres, { onDelete: "CASCADE" })
-  @JoinColumn({ name: "cityId" })
-  city!: City;
+  @ManyToOne(() => Day, (day) => day.theatres, { onDelete: "CASCADE" })
+  @JoinColumn({ name: "day_id" })
+  day!: Day;
 
-  @OneToMany(() => Screen, (screen) => screen.theatre, { eager: true })
+  @OneToMany(() => Screen, (screen) => screen.theatre, {
+    cascade: true,
+    eager: true,
+  })
   screens!: Screen[];
 }
 
 @Entity()
 export class Screen {
   @PrimaryGeneratedColumn("uuid")
-  id!: number;
+  id!: string;
 
   @Column()
   type!: string;
@@ -96,6 +118,6 @@ export class Screen {
   @ManyToOne(() => Theatre, (theatre) => theatre.screens, {
     onDelete: "CASCADE",
   })
-  @JoinColumn({ name: "theatreId" })
+  @JoinColumn({ name: "theatre_id" })
   theatre!: Theatre;
 }
