ec7fa968e2ea1b9d65730cda4cc311e9c074e4fd
priya <priyankbhalala1911@gmail.com>
2025-04-01 17:49:03 +0530

    Added movieNews entity

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,13 +4,15 @@ import { User } from "../models/user";
 import "dotenv/config";
 import { City, Day, Movie, Screen, Theatre } from "../models/movie";
 import { AddDStaticData } from "../utils/AddMovie";
+import { MovieNews } from "../models/movieNews";
+import { AddMovieNews } from "../utils/AddMovieNews";
 
 export const AppSourcedata = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
   synchronize: true,
   logging: false,
-  entities: [User, Movie, City, Theatre, Screen, Day],
+  entities: [User, Movie, City, Theatre, Screen, Day, MovieNews],
   schema: "public",
   ssl: {
     rejectUnauthorized: false,
@@ -21,6 +23,7 @@ export const initialDatabase = async () => {
   try {
     await AppSourcedata.initialize();
     // await AddDStaticData();
+    // await AddMovieNews();
     console.log("Database connection established successfully.");
   } catch (error) {
     console.log("Error connecting to the database", error);
