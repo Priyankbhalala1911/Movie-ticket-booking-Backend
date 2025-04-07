4bce1a43490bb61c76d3056b85d578a4ecdf1128
priya <priyankbhalala1911@gmail.com>
2025-04-07 14:50:13 +0530

    To added seat,screen and ticketbooking routes

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import { initialDatabase } from "./config/database";
 import UserRouter from "./routes/user";
 import MovieRouter from "./routes/movie";
 import MovieNewsRouter from "./routes/movieNews";
+import ScreenRouter from "./routes/screen";
+import PaymentRouter from "./routes/payment";
+import BookingRouter from "./routes/seatBooking";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
@@ -19,6 +22,9 @@ app.use(express.json());
 app.use("/", UserRouter);
 app.use("/movie", MovieRouter);
 app.use("/news", MovieNewsRouter);
+app.use("/screen", ScreenRouter);
+app.use("/payment", PaymentRouter);
+app.use("/api", BookingRouter);
 
 initialDatabase();
 app.listen(process.env.PORT || 8100, () =>
