3c72ab9a7fd6738df6ff71a3e2ad7ac3423af089
priya <priyankbhalala1911@gmail.com>
2025-04-16 11:42:09 +0530

    updated import file

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,13 +3,13 @@ import { DataSource } from "typeorm";
 import { User } from "../models/user";
 import "dotenv/config";
 import { City, Day, Movie, Screen, ShowTime, Theatre } from "../models/movie";
-import { AddDStaticData } from "../utils/Addedmovie/AddMovie";
+// import { AddDStaticData } from "../utils/Addedmovie/AddMovie";
 import { MovieNews } from "../models/movieNews";
-import { AddMovieNews } from "../utils/Addedmovie/AddMovieNews";
+// import { AddMovieNews } from "../utils/Addedmovie/AddMovieNews";
 import { Seat } from "../models/seat";
 import { SeatBooking } from "../models/seatBooking";
 import { ComingMovies } from "../models/comingMovies";
-import { AddComingMovies } from "../utils/Addedmovie/ComingMovies";
+// import { AddComingMovies } from "../utils/Addedmovie/ComingMovies";
 
 export const AppSourcedata = new DataSource({
   type: "postgres",
